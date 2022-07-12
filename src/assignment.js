14179fb23f37428925285b815e9b86f4f73bd81c
Isiaka Azeezat <87272869+Azeezahhh@users.noreply.github.com>
2022-07-12 17:35:12 +0000

    assigment 2

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,17 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let sum = 0
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+    sum += arrayOfNumbers[i];
+  }
+   return sum;
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment. sumOfNumbers =  sumOfNumbers;
+
+
+
+
 
 /**
  * Challenge - 2
@@ -28,9 +36,17 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   
+    let count = 0;
+        for (let i = 0; i < arrayOfNumbers.length; i++) {
+            if (arrayOfNumbers[i] % 2 === 0) {
+                count++;
+            }
+          }
+          return count;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
+
 
 /**
  * Challenge - 3
@@ -47,9 +63,15 @@ function countEvenNumbers(arrayOfNumbers) {
  * @returns Array the converted temperatures in Fahrenheit
  */
 function celsiusToFahrenheit(arrayOfNumbers) {
-    return arrayOfNumbers;
+  let tempInFehr = []
+  for (let i=0; i<arrayOfNumbers.length; i++) {
+    let convertToFahrenheit = arrayOfNumbers[i]*(9/5)+32;
+    let removeDecimalValue = Math.trunc(convertToFahrenheit);
+    tempInFehr.push(removeDecimalValue);
+  }
+  return tempInFehr
 }
-// assignment.celsiusToFahrenheit = celsiusToFahrenheit;
+assignment.celsiusToFahrenheit = celsiusToFahrenheit;
 
 
 // ========================
